e4a6d460cfe74fe69d20579989307cb6685476b8
linh-ngu <79113863+linh-ngu@users.noreply.github.com>
2022-09-05 21:41:58 -0500

    full game

---
+++ adversarialHangman.py
@@ -0,0 +1,49 @@
+import re
+
+with open(r"/Users/linhnguyen/Documents/personal projects/adversarialHangman/google-10000-english-usa-no-swears.txt","r+") as f:
+    allWords = f.read().split('\n')
+
+wordLength = 0
+while (wordLength < 1 or wordLength > len(max(allWords, key = len))):
+    wordLength = int(input("What word length do you want to play with? (1-18): "))
+
+possibleWords = [i for i in allWords if len(i) == wordLength]
+playerPattern = "_" * wordLength
+guessedLetters = set('_')
+guessedLetter = '_'
+
+while True:
+    searchSpace = dict()
+    print("-----------------------------------------------------------")
+    print("\nGuessed Letters:", guessedLetters)
+    while guessedLetter in guessedLetters:
+        guessedLetter = input(playerPattern + "\nGuess: ")
+        if guessedLetter in guessedLetters:
+            print("You already guessed this letter.")
+    guessedLetters.add(guessedLetter)
+
+    for word in possibleWords:
+        if playerPattern == "_" * wordLength:
+            pattern = "_" * wordLength
+        else:
+            pattern = playerPattern
+        res = [i for i in range(len(word)) if word.startswith(guessedLetter, i)]
+        for i in res:
+            pattern = pattern[:i] + guessedLetter + pattern[i+1:]
+        if pattern not in searchSpace:
+            searchSpace[pattern] = []
+        
+        searchSpace[pattern].append(word)
+
+    temp = -1
+    for patterns in searchSpace:
+        if len(searchSpace[patterns]) > temp:
+            temp = len(searchSpace[patterns])
+            playerPattern = patterns
+
+    possibleWords = searchSpace[playerPattern]
+
+    if "_" not in playerPattern:
+        print("-----------------------------------------------------------")
+        print("\nYou win! The word was " + "\"" + possibleWords[0] + "\"" + ".")
+        break
